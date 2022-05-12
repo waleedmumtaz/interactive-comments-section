2b6c3016123b00d617b7e89114acf093b3e1a360
Waleed <waleedthedev@gmail.com>
2022-05-12 07:42:09 +0500

    Add padding to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="">
+<footer class="pt-10 pb-2">
 	Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
 		>Frontend Mentor</a
 	>. Coded by <a href="https://github.com/waleedmumtaz">Waleed Mumtaz</a>.
